b6127868ebe293ef2c7305ba3cc75610aa851711
Nunwan <rob1bgaming@gmail.com>
2019-04-30 17:48:20 +0200

    Updating vertexshader to have a multicolor triangle

--- openGL/Main.cpp
+++ openGL/Main.cpp
@@ -5,17 +5,20 @@
 //shader source code in GLSC
 const char *vertexShaderSource = "#version 330 core\n" //Version of OpenGL
 "layout (location = 0) in vec3 aPos;\n" 
+"layout (location = 1) in vec3 aColor; \n"
+"out vec3 ourColor; \n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" //Position of shader
+"   gl_Position = vec4(aPos, 1.0);\n" //Position of shader
+"	ourColor = aColor;\n"
 "}\0";
 //fragment shader source code 
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"	FragColor = ourColor;\n"
+"	FragColor = vec4(ourColor, 1.0f);\n"
 "}\0 ";
 
 // Input function to close windows with escape 
@@ -121,10 +124,10 @@ int main()
 
 	//Triangle
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,  // top right
-		 0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
 	};
 
 	unsigned int indices[] = {  // note that we start from 0!
@@ -132,10 +135,9 @@ int main()
 	1, 2, 3    // second triangle
 	};
 	//id of buffer & array 
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	//Buffer generation : only 1
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 	glGenVertexArrays(1, &VAO);
 	
 	//link GL_ARRAY_BUFFER to VBO
@@ -145,20 +147,20 @@ int main()
 
 	//Bind the VAO 
 	glBindVertexArray(VAO);
-	//Attribute pointers to buff to VAO 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//Attribute pointers to buff to VAO  : position 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	//Attribute pointers to buff to VAO : color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	//BIND EBO 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
 
 	//RENDERING
 	//------------------------------------------------------------------
 
-
+	glUseProgram(shaderProgram);
 	//Window size
 	glViewport(0, 0, 800, 600);
 	while (!glfwWindowShouldClose(window))
@@ -168,17 +170,12 @@ int main()
 		//rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		//change the color 
-		float timeValue = glfwGetTime();
-		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 		// draw the triangle.
-		glUseProgram(shaderProgram);
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		
 		glBindVertexArray(VAO);
 	//	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); wireframe mode
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glBindVertexArray(0);
+	//	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);	
 		//swap the buffer & check events
 		glfwSwapBuffers(window);
 		glfwPollEvents();
