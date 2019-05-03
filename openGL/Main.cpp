4d1eeae9af27904c0b498eae4243a6963abb367c
Nunwan <rob1bgaming@gmail.com>
2019-05-03 18:06:47 +0200

    Add a translation

--- openGL/Main.cpp
+++ openGL/Main.cpp
@@ -1,6 +1,6 @@
 //					MAIN PROGRAM OF LEARNING OpenGL 
 //							BERTIN ROBIN
-//							TRY TO DO HIS BEST
+//					   	TRY TO DO HIS BEST
 
 // INCLUDE
 //------------------------------------------------------------------ 
@@ -217,7 +217,9 @@ int main()
 
 		//others
 		//Transformation : 
+		glm::vec3 vec(1.0f, 0.0f, 0.0f);
 		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::translate(trans, vec);
 		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
 		trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
 
