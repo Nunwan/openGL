5c21354b12fd49059759908875974a422747ea19
Nunwan <rob1bgaming@gmail.com>
2019-05-04 18:12:45 +0200

    Enable mouse rotation & zoom

--- openGL/Main.cpp
+++ openGL/Main.cpp
@@ -29,11 +29,30 @@
 //Global Variables
 //------------------------------------------------------------------ 
 
+
+//Screen Variables
+//------------------------------------------------------------------ 
+const int screenHeight = 600;
+const int screenWidth = 800;
+
+
 //Camera Variables
 //------------------------------------------------------------------ 
+
+//Translation 
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); // tricks for creating Right axis of Camera
+
+//Rotation 
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
+
 
 //Time Variables 
 //------------------------------------------------------------------ 
@@ -45,15 +64,14 @@ float lastFrame = 0.0f; // Time of last frame
 //------------------------------------------------------------------ 
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 //------------------------------------------------------------------ 
 
 
 int main()
 {
-	//Global parameters
-	int screenHeight = 600;
-	int screenWidth = 800;
 	//Initialisation of glfw 
 	//------------------------------------------------------------------
 
@@ -78,7 +96,13 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
+
+	//Configuration of Glfw
 	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); //Hide the cursor & enable catch mouse
+	glfwSetCursorPosCallback(window, mouse_callback); //Save mouse movements
+	glfwSetScrollCallback(window, scroll_callback); //save scroll movements
+
 	//Glad check
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -325,7 +349,7 @@ int main()
 		view = glm::lookAt(cameraPos,cameraPos + cameraFront, cameraUp);
 		//perpective matrix
 		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(45.0f), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+		projection = glm::perspective(glm::radians(fov), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
 		ourShader.setMat4("view", view);
 		ourShader.setMat4("projection", projection);
 
@@ -375,6 +399,7 @@ int main()
 	return 0;
 }
 
+
 // Input function to interact with the windows 
 void processInput(GLFWwindow *window)
 {	
@@ -392,7 +417,60 @@ void processInput(GLFWwindow *window)
 		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	
 }
+
+
+// Save mouse position & enable the camera to rotate
+void mouse_callback(GLFWwindow *window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f; // change this value to your liking
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
+
+//save the scroll of mouse & enable zoom
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (fov >= 1.0f && fov <= 45.0f)
+		fov -= yoffset;
+	if (fov <= 1.0f)
+		fov = 1.0f;
+	if (fov >= 45.0f)
+		fov = 45.0f;
+}
+
+
+
+
+
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
