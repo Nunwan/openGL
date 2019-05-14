c392393ea8cdf34e387b8b77feaf3088a510d6da
Nunwan <rob1bgaming@gmail.com>
2019-05-14 23:03:21 +0200

    Add Assimp library

--- openGL/Main.cpp
+++ openGL/Main.cpp
@@ -16,7 +16,6 @@
 #include <glm/glm.hpp> 
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 //library to open image in char
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
